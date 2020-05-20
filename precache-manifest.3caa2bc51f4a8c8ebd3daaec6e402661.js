df668ed9c527b7cec1e6bcccfd3e1e6f3be6309a
shayan74 <shayan_ebrahimi@live.com>
2020-05-21 01:11:28 +0300

    Updates

---
+++ precache-manifest.3caa2bc51f4a8c8ebd3daaec6e402661.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c3ae4cc4574087366786486464e349",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "f3d291bacfa213fda26a",
+    "url": "/myresume/static/css/main.b4d48bad.chunk.css"
+  },
+  {
+    "revision": "969651e1cb0a9cf10447",
+    "url": "/myresume/static/js/2.e6f02f57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myresume/static/js/2.e6f02f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d291bacfa213fda26a",
+    "url": "/myresume/static/js/main.6b1cca41.chunk.js"
+  },
+  {
+    "revision": "b7d19cf78f280fa66d2234df2807ea7c",
+    "url": "/myresume/static/js/main.6b1cca41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147a57315f8c4394fb58",
+    "url": "/myresume/static/js/runtime-main.b77aea2c.js"
+  },
+  {
+    "revision": "79d2130a15905424aea2ce6dd64bfb71",
+    "url": "/myresume/static/media/avatar.79d2130a.png"
+  }
+]);
